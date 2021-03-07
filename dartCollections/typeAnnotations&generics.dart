fe28250f8f4e06bba8812ab1ee44e7421b699ed8
Moonwalker <abhisk.mhra@gmail.com>
2021-03-07 12:07:57 +0530

    concept of type annotations and generics in dart

---
+++ dartCollections/typeAnnotations&generics.dart
@@ -0,0 +1,14 @@
+void main() {
+  var shoppingCart = <String>[];
+
+  shoppingCart.addAll(["Cake", "Apple", "Cookies", "Banana"]);
+  shoppingCart.add("Peanuts");
+  print(shoppingCart);
+  var person = <String, dynamic>{
+    'name': 'abhishek',
+    'age': 21,
+    'height': 5.7,
+  };
+
+  print(person);
+}
